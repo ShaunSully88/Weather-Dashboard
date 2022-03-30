ddc519e061bc57e98bee12e0a73ef330e89afd26
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-30 13:10:55 -0400

    tried using api Key as variable, code did not like how i input it. Removed as variable, input api key into url places. Getting responses from weather call as well as response.

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,13 @@ var cityContainerEl= document.querySelector("#main-container");
 var citySearchTerm = document.querySelector("#city-weather");
 var cityFormEl = document.querySelector("#city-form");
 var searchButton = document.getElementById("search-btn")
+var fiveDay = document.getElementById("five-day-card")
 var apiKey = "42e1f1d2c64b89323a4af79fe114ff93";
-var cityName = response.name;
-var latitude = response.coord.lat;
-var longitude = repsonse.coord.lon;
-var today = new Date();
-var date = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-var forecast = response.weather[0].description;
 var weatherToday = document.querySelector("#weather-today");
 var weatherTodayCard = document.querySelector("#weather-today-card");
+var today = new Date();
+var date = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -31,62 +29,71 @@ var formSubmitHandler = function (event) {
 
 
 var getWeather = function (cityInput) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?" + cityInput + "&exclude=hourly,minutely,alerts&appid=42e1f1d2c64b89323a4af79fe114ff93";
-
-    fetch(apiUrl).then(function(response) {
-        if(response.ok) {
-        response.json().then(function(data) {
-                    
-        });
-       } else {
-        alert("Error! " + response.statusText);
-       }
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&exclude=hourly,minutely,alerts&appid=42e1f1d2c64b89323a4af79fe114ff93";
+    console.log(apiUrl)
+    fetch(apiUrl).then(function(cityResponse) {
+        return cityResponse.json();
     })
-    .catch(function(error) {
-        alert("Unable to reach website");
-    });
+    .then(function(cityResponse) {
+
+    var cityName = cityResponse.name;
+    var latitude = cityResponse.coord.lat;
+    var longitude = cityResponse.coord.lon;
+    var forecast = cityResponse.weather[0].description;
+   
+
+    //weatherToday.textContent = "";
+    //fiveDay.textContent = "";
+
+
+    return fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely,alerts&appid=42e1f1d2c64b89323a4af79fe114ff93")
+    })
     console.log(fetch)
-
-    weatherToday.textContent = "";
+    // .then(function (response) {
+    //     return response.json();
+    // })
+    // .then(function (response) {
+    //     showWeather(response);
+    // });
 
 };
 
 
-var showWeather = function(cities, searchTerm) {
-    if(cities.length === 0) {
-        cityContainerEl.textContent = "No cities found!";
-        return;
-    }
+// var showWeather = function(cities, searchTerm) {
+//     if(cities.length === 0) {
+//         cityContainerEl.textContent = "No cities found!";
+//         return;
+//     }
     
-    citySearchTerm.textContent = searchTerm;
+//     citySearchTerm.textContent = searchTerm;
 
-    for (var i = 0; i < cities.length; i++) {
-        //var cityName = repos[i].owner.login + "/" + repos[i].name;
+//     for (var i = 0; i < cities.length; i++) {
+//         //var cityName = repos[i].owner.login + "/" + repos[i].name;
 
-        var cityEl = document.createElement("a");
-        cityEl.classList = "list-item flex-row justify-space-between align-center";
-        cityEl.setAttribute = ("href", " " + "");
+//         var cityEl = document.createElement("a");
+//         cityEl.classList = "list-item flex-row justify-space-between align-center";
+//         cityEl.setAttribute = ("href", " " + "");
 
-        var titleEl = document.createElement("span");
-        titleEl.textContent =  ""  ;
+//         var titleEl = document.createElement("span");
+//         titleEl.textContent =  ""  ;
 
-        cityEl.appendChild(titleEl);
+//         cityEl.appendChild(titleEl);
 
-        var statusEl = document.createElement("span");
-        statusEl.classList ="flex-row align-center";
+//         var statusEl = document.createElement("span");
+//         statusEl.classList ="flex-row align-center";
 
-        if(cities[i].open_issues_count > 0) {
-            statusEl.innerHTML = 
-            "<i class='fas fa-times status-icon icon-danger'></i>" + cities[i].open_issues_count + "issue(s)";
-        } else {
-            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
+//         if(cities[i].open_issues_count > 0) {
+//             statusEl.innerHTML = 
+//             "<i class='fas fa-times status-icon icon-danger'></i>" + cities[i].open_issues_count + "issue(s)";
+//         } else {
+//             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//         }
 
-        cityEl.appendChild(statusEl);
+//         cityEl.appendChild(statusEl);
 
-        cityContainerEl.appendChild(cityEl);
-    }
-};
+//         cityContainerEl.appendChild(cityEl);
+//     }
+//};
 
 
 console.log(getWeather)
